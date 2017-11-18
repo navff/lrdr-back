ad86a06b95ffae38bd2e58799b7e0c08d179e675
navff <var@33kita.ru>
2017-11-18 18:00:56 +0300

    Поигрался с сокетами

--- API/Controllers/ChatController.cs
+++ API/Controllers/ChatController.cs
@@ -4,43 +4,95 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using API.Common;
+using API.Operations;
 using Microsoft.Web.WebSockets;
+using Models.Operations;
 
 namespace API.Controllers
 {
     public class ChatController : ApiController
     {
-        public HttpResponseMessage Get(string username)
+        private UserOperations _userOperations;
+        private OrderOperations _orderOperations;
+
+        public ChatController(UserOperations userOperations, OrderOperations orderOperations)
         {
-            HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler(username));
+            _userOperations = userOperations;
+            _orderOperations = orderOperations;
+        }
+
+        //[RESTAuthorize]
+        public async Task<HttpResponseMessage> Get(int orderId, string token)
+        {
+            var user = await _userOperations.GetUserByTokenAsync(token);
+            if (user == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+
+            var order = await _orderOperations.GetAsync(orderId);
+            if (order == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+            if (user.Id != order.OwnerUserId && user.Id != order.CustomerUserId)
+            {
+                return new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+
+
+            HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler(orderId, User.Identity.Name));
             return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
         }
 
         class ChatWebSocketHandler : WebSocketHandler
         {
-            private static WebSocketCollection _chatClients = new WebSocketCollection();
-            private string _username;
+            private static Dictionary<int, WebSocketCollection> _chats = new Dictionary<int, WebSocketCollection>();
+            private WebSocketCollection _currentChat;
+            private string _userEmail;
 
-            public ChatWebSocketHandler(string username)
+            public ChatWebSocketHandler(int orderId, string userEmail)
             {
-                _username = username;
+                _userEmail = userEmail;
+                _currentChat = new WebSocketCollection();
+                if (!_chats.ContainsKey(orderId))
+                {
+                    _chats.Add(orderId, _currentChat);
+                }
+                
             }
 
             public override void OnOpen()
             {
                 base.OnOpen();
-                _chatClients.Add(this);
+                _currentChat.Add(this);
             }
 
             public override void OnMessage(string message)
             {
                 JavaScriptSerializer serializer = new JavaScriptSerializer();
-                var msg = serializer.Deserialize<SomeMessage>(message);
-                _chatClients.Broadcast(_username + " ^ " + message);
+                SomeMessage msg = new SomeMessage();
+                try
+                {
+                    msg = serializer.Deserialize<SomeMessage>(message);
+                }
+                catch (Exception e)
+                {
+                    // Интересно, что делать, если клиент прислал говно?
+                    // пока что просто глушим
+                    Console.WriteLine(e);
+                    return;
+                }
+
+                _currentChat.Broadcast(_userEmail + " ^ " + msg.Text);
             }
+
         }
 
         class SomeMessage
